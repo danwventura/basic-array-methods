c781ff12157438ac91da94d4a0a04ce55d3bb24a
Dan Ventura <danwventura@icloud.com>
2016-03-02 20:14:46 -0600

    changed id references for variables

--- events.js
+++ events.js
@@ -1,11 +1,17 @@
 //Making variables to be used later
 var output = document.getElementById("output-target");
 var keypress = document.getElementById("keypress-input");
-var colorBtn = document.getElementById("turnblue");
-var hulkifyBtn = document.getElementById("largefont");
-var addBorderBtn = document.getElementById("captureit");
-var roundBorder = document.getElementById("borderrounded");
-var header = document.getElementById("myheader");
-var title = document.getElementById("mytitle")
-var article = document.getElementsByName("articles")
+var guineaPig = document.getElementById("guinea-pig")
+var colorBtn = document.getElementById("add-color");
+var hulkifyBtn = document.getElementById("make-large");
+var addBorderBtn = document.getElementById("add-border");
+var roundBorder = document.getElementById("add-rounding");
+var header = document.getElementById("page-header");
+var title = document.getElementById("page-title")
+var article = document.getElementsByName("article-section")
 //getElementsByName --> [always returns array]
+
+console.log(output);
+
+//Adding event listener for click --> "You clicked (section) section"
+// article.addEventListener("click", function ())
