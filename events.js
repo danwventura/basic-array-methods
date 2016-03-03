653d090dcf18b4dabf873a45dfae2a4b33f79af7
Dan Ventura <danwventura@icloud.com>
2016-03-02 19:51:41 -0600

    added DOM variable

---
+++ events.js
@@ -0,0 +1,11 @@
+//Making variables to be used later
+var output = document.getElementById("output-target");
+var keypress = document.getElementById("keypress-input");
+var colorBtn = document.getElementById("turnblue");
+var hulkifyBtn = document.getElementById("largefont");
+var addBorderBtn = document.getElementById("captureit");
+var roundBorder = document.getElementById("borderrounded");
+var header = document.getElementById("myheader");
+var title = document.getElementById("mytitle")
+var article = document.getElementsByName("articles")
+//getElementsByName --> [always returns array]
